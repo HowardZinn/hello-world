83240aad37a7ac808687fbbdd8b1f7ff9bb61fa5
HowardZinn <34632000+HowardZinn@users.noreply.github.com>
2017-12-17 18:01:43 -0500

    Update main.m

--- main.m
+++ main.m
@@ -3,5 +3,9 @@
 int main(){
     printf("Hello, world!\n");
     
+    printf("My name is:\n");
+    printf("Howard Zinn\t"); // this is how you make tabs
+    printf("I'm an author\a\n"); // '\a' is for alerts, couldn't get it to sound on my pc though
+    
     return 0;
 }
