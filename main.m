ef8e4b0908691102f2a30e40ddb437ff93a47188
HowardZinn <34632000+HowardZinn@users.noreply.github.com>
2017-12-18 21:32:37 -0500

    Update main.m

--- main.m
+++ main.m
@@ -8,11 +8,11 @@ int main(){
     printf("Hello, world!\n");
     
     printf("My name is:\n");
-    printf("Howard Zinn\t"); // this is how you make tabs
+    printf("Howard Zinn\t"); // '\t' is how you make tabs
     printf("I'm an author\a\n"); // '\a' is for alerts, couldn't get it to sound on my pc though
     
     
-    printf("I liked Star %s, episode %d.\n", "Wars", 8);
+    printf("I liked Star %s, episode %d.\n", "Wars", 8); 
     printf("I liked Star %s, parts %d, %d, and %d.\n", "Trek", 2, 4, 6);
     printf("But I wish there was a Star Wars episode %f, about the origins of Yoda.\n", 0.1);
     printf("Sorry, I mean I wish there was an episode %.1f. Who needs all those extra 0's?\n", 0.1);
